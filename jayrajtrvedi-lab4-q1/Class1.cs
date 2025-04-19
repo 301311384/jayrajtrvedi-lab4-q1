0a8532dc912516a02c85223380757113b74277c6
Devkumar Patel <dpat1072@my.centennialcollege.ca>
2025-04-19 14:57:33 -0400

    first commit

---
+++ jayrajtrvedi-lab4-q1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jayrajtrvedi_lab4_q1
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return false;
+            return char.IsUpper(str[0]);
+        }
+    }
+}
